24049bb74b2a8413f778c4a53705be9407524edd
SH <email@email.com>
2017-05-03 12:05:15 -0500

    added about to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+/*end of home page*/
+
+// GET about page
+router.get('/about', function(req, res, next) {
+    res.render('about');
 });
 
 module.exports = router;
