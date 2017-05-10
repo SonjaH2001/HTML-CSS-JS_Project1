5ac5705201a11a69b527b3f920e5ff5613bb5652
SH <email@email.com>
2017-05-10 12:54:29 -0500

    added model>order.js css> trying to get @keyframe working order_page.hbs updated inputs

---
+++ models/order.js
@@ -0,0 +1,20 @@
+//use mongoose
+var mongoose = require('mongoose');
+// var uniqueValidator = require('mongoose-unique-validator');
+
+// represents each order
+var orderSchema = new mongoose.Schema({
+    firstName: { type: String, required: true, uniqueCaseInsensitive: true },
+    lastName: { type: String, required: true, uniqueCaseInsensitive: true },
+    streetAddress: { type: String, required: true, uniqueCaseInsensitive: true },
+    city: { type: String, required: true, uniqueCaseInsensitive: true },
+    state: { type: String, required: true, uniqueCaseInsensitive: true },
+    zipCode: { type: String, required: true, uniqueCaseInsensitive: true },
+    email: { type: String, required: true, uniqueCaseInsensitive: true },
+    phone: { type: String, required: false, uniqueCaseInsensitive: true }
+});
+
+var Order = mongoose.model('Order', orderSchema);
+    orderSchema.plugin(uniqueValidator);
+
+module.exports = Order;
